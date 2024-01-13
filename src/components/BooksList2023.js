8e6e35ae7cebc6d25ddee608ee9448ac81c82fc3
Dichik <omeluan.dima@gmail.com>
2024-01-13 16:06:43 +0200

    add books and experience

--- src/components/BooksList2023.js
+++ src/components/BooksList2023.js
@@ -2,7 +2,15 @@ import React from 'react';
 import './BooksList.css';
 
 const books = [
-    { year: 2023, title: 'To be added', author: 'Added' },
+    { year: 2023, title: 'Напролом', author: 'Раян Голідей' },
+    { year: 2023, title: 'Розгадка геніальності', author: 'Рон Фрідман' },
+    { year: 2023, title: 'Creative personal branding', author: 'Jürgen Salenbacher' },
+    { year: 2023, title: 'Нові Стоїки', author: 'Массімо Пільюччі та Ґреґорі Лопес' },
+    { year: 2023, title: 'Дар', author: 'Едіт Еґер' },
+    { year: 2023, title: 'Зелене світло', author: 'Метью Макконагі' },
+    { year: 2023, title: 'The Subtle Art of Not Giving a F*ck', author: 'Mark Manson' },
+    { year: 2023, title: 'Мізері', author: 'Стівен Кінг' },
+    { year: 2023, title: '...', author: '...' }
 ];
 
 function BooksList2023() {
--- src/components/Experience.js
+++ src/components/Experience.js
@@ -4,20 +4,50 @@ import './Experience.css';
 function Experience() {
     const experiences = [
         {
-            title: "TBD",
-            company: "TBD",
-            dates: "Jan TBD - Present",
-            description: "Description of your responsibilities and achievements."
-        }
+            title: "SE",
+            company: "...",
+            dates: "Present",
+            description: "TBD."
+        },
+        {
+            title: "Machine Learning Intern",
+            company: "Center of Responsible AI @ NYU",
+            dates: "Sep 2023 - Dec 2023",
+            description: "TBD."
+        },
+        {
+            title: "Co-Founder",
+            company: "Hearify",
+            dates: "March 2023 - Nov 2023",
+            description: "TBD."
+        },
+        {
+            title: "Software Engineer Intern",
+            company: "Huawei",
+            dates: "June 2023 - July 2023",
+            description: "TBD."
+        },
+        {
+            title: "Machine Learning Engineer",
+            company: "Harmix",
+            dates: "Apr 2023 - Aug 2023",
+            description: "TBD."
+        },
+        {
+            title: "Junior Software Engineer",
+            company: "Micro Focus",
+            dates: "Nov 2021 - Dec 2022",
+            description: "TBD."
+        },
     ];
 
     return (
         <div className='experience-container'>
             <h1>Experience</h1>
             {experiences.map((exp, index) => (
-                <div key={index}>
-                    <h2>{exp.title}</h2>
-                    <h3>{exp.company}</h3>
+                <div className='experience-box' key={index}>
+                    <h3>{exp.title}</h3>
+                    <h4>{exp.company}</h4>
                     <p>{exp.dates}</p>
                     <p>{exp.description}</p>
                 </div>
